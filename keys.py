81e822c0e5e2a076244d89b2db8cf67b01ff6b88
Mizan <mdmizanali04@gmail.com>
2021-05-22 18:46:17 +0530

    Added keys.py

---
+++ keys.py
@@ -0,0 +1,7 @@
+# https://www.youtube.com/watch?v=qg-oboAY8rM
+api_key = # Your own API Key here
+api_secret = # Your API Secret Key here
+
+# In order to get an alert email you have to have a dummy email account that send those alert to your main email.
+sendersEmail = # Dummy Email
+sendersPassword = # Dummy Email password
